19328f11f23b2fd1da3778c13cea4550b1e82711
caiolul <caiolucena.il@gmail.com>
2024-01-22 00:01:40 -0300

    Refactor video conversion and quality improvement
    Add delete_files.py for file deletion

--- converter.py
+++ converter.py
@@ -1,31 +1,67 @@
 from moviepy.editor import VideoFileClip
 import numpy as np
-
+from moviepy.video.fx.all import colorx
 from PIL import Image
 
-def resize_clip(clip, height, method):
+
+def resize_clip(clip, height, upscale, method, size_default=None):
     def resize(img):
         img_pil = Image.fromarray(img)
-        img_resized = img_pil.resize((int(img.shape[1] * height / img.shape[0]), height), resample=method)
+        if size_default is not None:
+            size = size_default
+        else:
+            if upscale == True:
+                size = (int(img.shape[1] * height / img.shape[0]), height)
+            else:
+                size = (int(img.shape[1] / (img.shape[0] / height)), height)
+
+        img_resized = img_pil.resize(size, resample=method)
         return np.array(img_resized)
-    
+
     return clip.fl_image(resize)
+
 
 def convert_avi_to_mkv_fast(input_file, output_file):
     video_clip = VideoFileClip(input_file)
-    video_clip.write_videofile(output_file, codec="libx264", audio_codec="aac", preset="ultrafast", threads=4)
+    video_clip.write_videofile(
+        output_file, codec="libx264", audio_codec="aac", preset="ultrafast", threads=4
+    )
 
 
 def convert_avi_to_mkv_slow(input_file, output_file):
     video_clip = VideoFileClip(input_file)
     # video_clip_resized = resize_clip(video_clip, height=2160, method=Image.BICUBIC)  # Ou use Image.
-    video_clip.write_videofile(output_file, codec="libx264", audio_codec="aac", bitrate="30M", preset="slow", threads=4)
+    video_clip.write_videofile(
+        output_file,
+        codec="libx264",
+        audio_codec="aac",
+        bitrate="30M",
+        preset="slow",
+        threads=4,
+    )
     # video_clip_resized.write_videofile(output_file, codec="libx264", audio_codec="aac", bitrate="30M", preset="slow", threads=4)
 
 
+def improve_quality_test(input_file, output_file):
+    clip = VideoFileClip(input_file)
+    # Configs de contrate e brilho
+    clip = colorx(clip, factor=1.5)  # Fator de contraste
+    clip = clip.fx(lambda c: c.fl_image(lambda frame: frame * 1.2))  # Ajuste de brilho
 
-# if __name__ == "__main__":
-#     input_video = "./Homem.Aranha.E01.Noite.Do.Lagarto.avi"
-#     output_video = "./Homem.Aranha.E01.Noite.Do.Lagarto_4k.mkv"
+    # Ajuste de cor
+    clip = clip.fx(colorx, factor=1.5)
 
-#     convert_avi_to_mkv(input_video, output_video)
+    # Filtro de nitidez
+    clip = resize_clip(clip, height=1080, upscale=True, method=Image.BICUBIC)
+    clip = resize_clip(clip, height=480, upscale=False, method=Image.BICUBIC)
+    clip = resize_clip(
+        clip, height=None, upscale=True, method=Image.BICUBIC, size_default=clip.size
+    )
+    clip.write_videofile(
+        output_file,
+        codec="libx264",
+        audio_codec="aac",
+        bitrate="30M",
+        preset="slow",
+        threads=4,
+    )
---
+++ delete_files.py
@@ -0,0 +1,18 @@
+# import os
+
+# folder_videos = input("Digite o caminho da pasta: ")
+
+# filenames = [file for file in os.listdir(folder_videos) if file.endswith(".avi")]
+
+# for filename in filenames:
+#     print(filename)
+#     if os.path.exists(os.path.join(folder_videos, filename)):
+#         print("The file exists")
+#         print("Deleting the file...")
+#         os.remove(os.path.join(folder_videos, filename))
+#         print("The file has been deleted")
+
+#         # os.remove(filename)
+#     else:
+#         print("The file does not exist") 
+
